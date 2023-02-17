a40878ffb31587e95a0fc7bd505ca267a99c9a55
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-02-17 22:22:20 +0900

    ADD minimum i18n

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
 
 function Header() {
+  const { t, i18n } = useTranslation();
     return (
       <header className="w-full container mx-auto">
         <div className="flex flex-col items-center py-12">
             <Link to="/" className="font-bold text-gray-800 uppercase hover:text-gray-700 text-5xl">
-            Minimal Blog
+            {t('title')}
             </Link>
             <p className="text-lg text-gray-600">
             Lorem Ipsum Dolor Sit Amet
