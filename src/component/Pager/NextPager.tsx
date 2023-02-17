a8e8ef84f19a94d83a87f562f3635bb87d88fcf4
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-02-18 01:45:07 +0900

    add empty translation.json

--- src/component/Pager/NextPager.tsx
+++ src/component/Pager/NextPager.tsx
@@ -1,6 +1,6 @@
 import { Link,  } from "react-router-dom";
 
-import { useBasePath } from "../../server/utils/router";
+import { useBasePath } from "../../utils/router";
 
 type Props = {page: number}
 
