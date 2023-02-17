85173e7ba1bbb3d1e345af26ab1d406a15924c23
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-02-18 02:10:42 +0900

    fix

--- src/server/api/newArticles.ts
+++ src/server/api/newArticles.ts
@@ -1,5 +1,4 @@
 const getNewArticlesApi = (page: string):string => {
-  console.log("process.env.REACT_APP_BASE_URL", process.env.REACT_APP_BASE_URL)
   return process.env.REACT_APP_BASE_URL + "/new" + "/" + page
 }
 
