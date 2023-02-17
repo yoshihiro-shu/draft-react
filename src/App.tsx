7e9edbc8addfd76193c239bb301dedb51e36de2d
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-02-18 02:40:12 +0900

    Refactor router

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from 'react-router-dom';
 
 import './App.css';
 
-import Layout from './component/Layout';
-import TopPage from './pages/TopPage';
-import NewArticles from './pages/NewArticles';
-import AritcleDetail from './pages/ArticleDetail';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { index: true, element: <TopPage /> },
-      {
-        path: "new/:page",
-        element: <NewArticles />,
-      },
-      {
-        path: "articles/:id",
-        element: <AritcleDetail />,
-      },
-    ],
-  },
-]);
-
+import router from './router';
 
 const App: React.FC = () => {
   return (
---
+++ src/router.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Layout from './component/Layout';
+import TopPage from './pages/TopPage';
+import NewArticles from './pages/NewArticles';
+import AritcleDetail from './pages/ArticleDetail';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <TopPage /> },
+      {
+        path: "new/:page",
+        element: <NewArticles />,
+      },
+      {
+        path: "articles/:id",
+        element: <AritcleDetail />,
+      },
+    ],
+  },
+]);
+
+export default router;
