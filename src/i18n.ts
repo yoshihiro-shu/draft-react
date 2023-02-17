b5b68940275a2db916df108438b8bb39ca7fd1ad
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-02-18 02:17:44 +0900

    Migrate to translation json for i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,30 +1,15 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import enTranslation from "./locales/en/translation.json"
+import jaTranslation from "./locales/ja/translation.json"
+
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
-  en: {
-    translation: {
-      "title": "Tech Blog en",
-      "description": "Lorem Ipsum Dolor Sit Amet",
-      "home": "HOME",
-      "about": "ABOUT",
-      "latestArticles": "Latest",
-      "copyright": "© MYBLOG.COM",
-    }
-  },
-  ja: {
-    translation: {
-      "title": "Tech Blog ja",
-      "description": "Lorem Ipsum Dolor Sit Amet",
-      "home": "HOME",
-      "about": "ABOUT",
-      "latestArticles": "新着記事",
-      "copyright": "© MYBLOG.COM",
-    }
-  }
+  en: enTranslation,
+  ja: jaTranslation
 };
 
 i18n
