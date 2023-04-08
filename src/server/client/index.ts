10aed5491fe8aac9f64a351b4e810ce79097ff6c
yoshihiro-shu <yoihongzhu0321@gmail.com>
2023-04-08 14:55:01 +0900

    ADD min csrf security

--- src/server/client/index.ts
+++ src/server/client/index.ts
@@ -1,3 +1,4 @@
+import Cookies from "js-cookie";
 import APIResponse from "./types";
 
 class ApiClient {
@@ -9,13 +10,22 @@ class ApiClient {
 
   public async Get<T>(api: string): Promise<APIResponse<T>> {
     const endpoint: string = this.backendAPI + api
-    return await fetch(endpoint)
-      .then(res => {
-        if (!res.ok) {
-          throw new Error(res.statusText)
-        }
-        return res.json() as Promise<APIResponse<T>>
-      })
+    return await fetch(endpoint, {
+      method: 'GET',
+      // For Set-Cookie
+      credentials: "include",
+    })
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.statusText)
+      }
+      // TODO recoilで管理したい
+      if (res.headers.get("X-Csrf-Token") !== "") {
+        Cookies.set('X-Csrf-Token', res.headers.get("X-Csrf-Token") as string)
+      }
+
+      return res.json() as Promise<APIResponse<T>>
+    })
   }
 
   public async Post<T>(api: string, data: { [key: string]: any }): Promise<APIResponse<T>> {
@@ -23,9 +33,10 @@ class ApiClient {
     return fetch(endpoint, {
       method: 'POST',
       mode: 'cors',
+      credentials: "include",
       headers: {
         'Accept': 'application/json;charset=utf-8',
-        'Content-Type': 'application/json;charset=utf-8',
+        'X-Csrf-Token': Cookies.get('X-Csrf-Token') as string,
       },
       body: JSON.stringify(data),
     })
